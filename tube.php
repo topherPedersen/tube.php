f8ebe889cfda4436a62542596b4b2d066d5a20a9
crispytx <crispytx@users.noreply.github.com>
2016-05-07 10:32:11 -0500

    added functionality
    New functionality: when the database of movies reaches 9 movies this script will remove the oldest movie from the database. Still need to write more code to remove the mp4 and html5 video files from the filesystem, not just the database.

--- tube.php
+++ tube.php
@@ -1,10 +1,58 @@
 <?php
-    $databaseName = "DATABASE-NAME-GOES-HERE";
-    $admin = "ADMIN-NAME-GOES-HERE";
-    $password = "PASSWORD-GOES-HERE";
-    function echoJSON($databaseName, $admin, $password) {
+            // bubble sort & delete oldest movie
+            $databaseName = "DATABASE-NAME-GOES-HERE";
+            $admin = "ADMIN-NAME-GOES-HERE";
+            $password = "PASSWORD-GOES-HERE";
+	        $tableName = "TABLE-NAME-GOES-HERE";
+	
+			// Create a Multi Dimensional Array of Movie Data (time, title, filename)
+			$mysqli = new mysqli("localhost", $admin, $password, $databaseName);
+            $query = "SELECT * FROM $tableName;";
+            $result = $mysqli->query($query);
+            $movieCount = 0;
+            while ($row = $result->fetch_assoc()) {
+	            global $movieCount;
+                $movieCount++;
+	            $movie[$movieCount]['time'] = $row['time'];
+                $movie[$movieCount]['title'] = $row['title'];
+                $movie[$movieCount]['filename'] = $row['filename'];
+            }
+            $mysqli->close();
+	        
+			
+			//echo "$movieCount <br>";
+			
+			if ($movieCount >= 9) {
+	            // bubble sort $movie[]['time'] to retrieve oldest movie unix epoch time
+	            $keepSorting = true;
+                while ($keepSorting == true) {
+	                global $movie;
+                    $keepSorting = false;
+                    for ($i = 1; $i < $movieCount; $i++) {
+                        $a = $i;
+	                    $b = $i + 1;
+	                    if ($movie[$a]['time'] > $movie[$b]['time']) {
+		                    global $keepSorting;
+		                    $keepSorting = true;
+	                        $placeholder = $movie[$b]['time'];
+		                    $movie[$b]['time'] = $movie[$a]['time'];
+		                    $movie[$a]['time'] = $placeholder;
+	                    }  
+                    }
+                }
+				$oldestMovie = $movie[1]['time'];
+				//echo "$oldestMovie";
+				mysql_connect('localhost', $admin, $password);
+                mysql_select_db($databaseName) or die("Unable to select database");
+		        $query = "DELETE FROM $tableName WHERE time = $oldestMovie;";
+                mysql_query($query);
+                mysql_close($databaseName);
+			}
+			// movies sorted, oldest deleted
+
+    function echoJSON($databaseName, $admin, $password, $tableName) {
         $mysqli = new mysqli("localhost", $admin, $password, $databaseName);
-        $query = "SELECT * FROM tubeTable;";
+        $query = "SELECT * FROM $tableName;";
         $result = $mysqli->query($query);
         $movieCount = 0;
         while ($row = $result->fetch_assoc()) {
@@ -29,9 +77,10 @@
         }
         $mysqli->close();
     }
-    function uploadFile($databaseName, $admin, $password) {
+    function uploadFile($databaseName, $admin, $password, $tableName) {
         $fileTitle = $_POST['file2'];
         $fileName= $_FILES["file1"]["name"];
+		$fileTime = time();
         $fileTmpLoc = $_FILES["file1"]["tmp_name"];
         $fileType = $_FILES["file1"]["type"];
         $fileSize = $_FILES["file1"]["size"];
@@ -45,7 +94,7 @@
             echo "UPLOAD COMPLETE: $fileName uploaded successfully"; // $fileName uploaded successfully
             mysql_connect('localhost', $admin, $password);
             mysql_select_db($databaseName) or die("Unable to select database");
-            $query = "INSERT INTO tubeTable VALUES (\"$fileTitle\", \"$fileName\");";
+            $query = "INSERT INTO $tableName VALUES (\"$fileTime\", \"$fileTitle\", \"$fileName\");";
             mysql_query($query);
             mysql_close($databaseName);
             // ------------------------------------------------------------------------------------------------
@@ -57,13 +106,14 @@
             fwrite($myfile, $txt);
             fclose($myfile);
             // ------------------------------------------------------------------------------------------------
+			
         } else {
             echo "PC LOAD LETTER"; // move_uploaded_file function failed
         }
     }
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        echoJSON($databaseName, $admin, $password);
+        echoJSON($databaseName, $admin, $password, $tableName);
     } else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        uploadFile($databaseName, $admin, $password);
+        uploadFile($databaseName, $admin, $password, $tableName);
     }
 ?>
